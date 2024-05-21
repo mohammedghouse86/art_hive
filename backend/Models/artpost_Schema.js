f4b4803701eac7f5dc02d9efc6ce086022c42503
Ghouse Mohammed <mohammedghouse86@gmail.com>
2024-05-21 22:17:27 +0530

    API for adding comments and likes

--- backend/Models/artpost_Schema.js
+++ backend/Models/artpost_Schema.js
@@ -6,6 +6,10 @@ const artpostSchema = new Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user' // this is to store the user --> ID --> JWT
     },
+    username: { 
+        type: String, 
+        required: true 
+    },
     filename: { 
         type: String, 
         required: true 
@@ -21,7 +25,18 @@ const artpostSchema = new Schema({
     imageBase64: { 
         type: String, 
         required: true 
-    }
+    },
+    bid_amount: { 
+        type: String
+    },
+    comments:{ commentorname:
+        {type: String,
+        } ,
+        hiscomment: {type: String,
+        } 
+    },
+    likes: { 
+        type: Number}
 });
 
 module.exports = mongoose.model('artpost', artpostSchema);
--- backend/Routes/upload_pics.js
+++ backend/Routes/upload_pics.js
@@ -12,29 +12,35 @@ const upload = multer({ storage });
 router.post('/art', getUser, upload.single('file'), [
     body('filename', 'Enter a valid title').isLength({ min: 3 }),
     body('contentType', 'Enter a valid description').isLength({ min: 3 }),
-    body('tag', 'Enter a tag').isLength({ min: 3 })
+    body('tag', 'Enter a tag').isLength({ min: 3 }),
+    body('bid_amount', 'Enter a bid_amount').isNumeric(),
+
 ], async (req, res) => {
     try {
         const { originalname, mimetype, buffer } = req.file;
         const imageBase64 = buffer.toString('base64');
-        const { filename, contentType, tag } = req.body;
-        const user = req.user.id; // Correctly assign user from req.user
-
+        const { filename, contentType, tag, bid_amount,username } = req.body;
+        const user = req.user.is; // Correctly assign user from req.user
+        const username1 = req.user.name; // Correctly assign user from req.user
+        console.log(user,username1)
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const note = new artpost({
-            user,
-            filename: originalname,
-            contentType: mimetype,
-            tag,
-            imageBase64
+        const new_artpost = new artpost({
+            user:user,
+            username:username,
+            filename: filename,
+            contentType: contentType,
+            tag:tag,
+            imageBase64:imageBase64,
+            bid_amount:bid_amount,
+            likes:0
         });
 
-        const savedNote = await note.save();
-        res.json(savedNote);
+        const saved_new_artpost = await new_artpost.save();
+        res.json(saved_new_artpost);
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({ errors: 'Internal Server Error' });
@@ -60,5 +66,57 @@ router.get('/file/:id', getUser, async (req, res) => {
     }
 });
 
+// 3. Route to add comment to a post
+router.post('/file_addComment/:id', getUser, async (req, res) => {
+    try {
+        const fileId = req.params.id;
+        const file = await artpost.findById(fileId);
+
+        if (!file) {
+            return res.status(404).json({ error: 'File not found' });
+        }
+
+        const {commentorname,hiscomment } = req.body;
+        const saved_new_artpost = {comments:{commentorname:null,hiscomment:null}};
+        {saved_new_artpost.comments.commentorname = commentorname};            
+        {saved_new_artpost.comments.hiscomment = hiscomment};      
+
+
+        // find the note to be updated and update it
+        let new_artpost = await artpost.findByIdAndUpdate(req.params.id, {$set:saved_new_artpost}, {new:true})
+        res.json({new_artpost});
+
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// 4. Route to add like to a post
+router.post('/file_addlike/:id', getUser, async (req, res) => {
+    try {
+        const fileId = req.params.id;
+        const file = await artpost.findById(fileId);
+
+        if (!file) {
+            return res.status(404).json({ error: 'File not found' });
+        }
+
+        const likes = 1;
+        console.log(file);
+        const saved_new_artpost = {likes:likes+file.likes}; 
+
+
+        // find the note to be updated and update it
+        let new_artpost = await artpost.findByIdAndUpdate(req.params.id, {$set:saved_new_artpost}, {new:true})
+        res.json({new_artpost});
+
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
